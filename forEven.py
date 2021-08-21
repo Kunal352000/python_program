d480e2f6bd98b62f51e22b325bee7186a38c3fad
Kunal Joshi <57366074+Kunal352000@users.noreply.github.com>
2021-08-21 17:17:11 +0530

    Add files via upload

---
+++ forEven.py
@@ -0,0 +1,4 @@
+num=int(input("Enter the numbers:"))
+for i in range(2,num+1):
+    if i%2==0:
+        print(i)
---
+++ remCoffi.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the value of x:"))
+y=int(input("Enter the value of y:"))
+rem=x%y
+cof=x//y
+print(rem)
+print(cof)
